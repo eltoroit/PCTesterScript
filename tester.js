7d220b317baaeccd9c942f7fa565d357a85c82ae
Andres Perez <andresperezf@yahoo.com>
2019-10-31 16:51:53 -0700

    TEST

--- tester.js
+++ tester.js
@@ -111,6 +111,7 @@ function spawnCommand(instruction) {
 
 	var process;
 	try {
+		instruction.Command__c = JSON.parse(instruction.Command__c);
 		if (instruction.Command__c.params && instruction.Command__c.params !== "") {
 			process = spawn(instruction.Command__c.cmd, instruction.Command__c.params);
 		} else {
