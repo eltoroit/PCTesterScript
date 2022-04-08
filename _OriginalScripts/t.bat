b058cf31955a30eab221715ac5b0f06d796aead3
Andres Perez <aperez@salesforce.com>
2022-04-08 12:22:41 -0400

    TEST

--- _OriginalScripts/t.bat
+++
@@ -1,6 +0,0 @@
-REM *** Must Go In The Image ***
-REM Place this file in the C:\User\Admin folder which is the default folder that gets opened when running CMD.
-REM This allows to just open the command prompt and type T <Enter>
-
-CD C:\TH
-test.bat
