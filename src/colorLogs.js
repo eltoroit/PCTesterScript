763097ebb1f369c66fcf387cf576944e863d23ad
Your Name <you@example.com>
2023-03-17 15:28:03 -0400

    Project bolierplate is working :-)

--- src/colorLogs.js
+++ src/colorLogs.js
@@ -1,8 +1,6 @@
-"use strict";
-
+// Define variables
 var showTimestamp = false;
 var showLineNumbers = false;
-
 let clearScreenCode = "\x1B[2J";
 
 // Color Modes
@@ -35,75 +33,82 @@ let colorBgMagenta = "\x1b[45m";
 let colorBgCyan = "\x1b[46m";
 let colorBgWhite = "\x1b[47m";
 
-module.exports = {
-	setDebug: function(isDebug) {
+export default class ColorLog {
+	static setDebug(isDebug) {
 		showTimestamp = isDebug;
 		showLineNumbers = isDebug;
-	},
-	getTime: function() {
+	}
+
+	static getTime() {
 		var date = new Date();
-        var hour = date.getHours();
-        var minutes = date.getMinutes();
-        var seconds = date.getSeconds();
-        var milliseconds = date.getMilliseconds();
-		var strTime = '[' +
-			((hour < 10) ? '0' + hour: hour) + ':' +
-			((minutes < 10) ? '0' + minutes: minutes) + ':' +
-			((seconds < 10) ? '0' + seconds: seconds) + '.' + ('00' + milliseconds).slice(-3) +
-		']';
-		
+		var hour = date.getHours();
+		var minutes = date.getMinutes();
+		var seconds = date.getSeconds();
+		var milliseconds = date.getMilliseconds();
+		var strTime =
+			"[" + (hour < 10 ? "0" + hour : hour) + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds) + "." + ("00" + milliseconds).slice(-3) + "]";
+
 		return strTime;
-    },
-    getTrace: function(offset) {
+	}
+
+	static getTrace(offset) {
 		var prefix;
-		
-        if (!offset) offset = 0;
+
+		if (!offset) offset = 0;
 
 		prefix = "";
 		if (showTimestamp) {
 			prefix += this.getTime();
 		}
-        if (showLineNumbers) {
-            try {
-                throw new Error();
-            } catch (e) {
-                if (typeof e.stack === 'string') {
-                    var linePart = 1;
+		if (showLineNumbers) {
+			try {
+				throw new Error();
+			} catch (e) {
+				if (typeof e.stack === "string") {
+					var linePart = 1;
 
-                    var lines = e.stack.split('\n');
-                    var line = lines[3 + offset];
+					var lines = e.stack.split("\n");
+					var line = lines[3 + offset];
 
-                    // console.log(colorBgBlack + colorBright + colorFgMagenta  + line + colorReset);
-                    if (line.toLowerCase().indexOf("c:\\th\\") > 0) linePart++;
-                    prefix += "[" + line.split(':')[linePart] + "]";
-                }
-            }
-        }
+					// console.log(colorBgBlack + colorBright + colorFgMagenta  + line + colorReset);
+					if (line.toLowerCase().indexOf("c:\\th\\") > 0) linePart++;
+					prefix += "[" + line.split(":")[linePart] + "]";
+				}
+			}
+		}
 		return prefix + (prefix.length > 1 ? ": " : "");
-    },
-    error: function (msg, offset) {
-        if (!offset) offset = 0;
-        console.log(colorBgBlack + colorBright + colorFgRed + this.getTrace() + msg + colorReset);
-    },
-    debug: function (msg) {
-        console.log(colorBgBlack + colorBright + colorFgGray + this.getTrace() + msg + colorReset);
-    },
-    info: function (msg) {
-        console.log(colorBgBlack + colorBright + colorFgWhite + this.getTrace() + msg + colorReset);
-    },
-    success: function (msg) {
-        console.log(colorBgBlack + colorBright + colorFgGreen + this.getTrace() + msg + colorReset);
-    },
-    promptMsg: function (msg) {
-        console.log(this.getPromptMsg(msg));
-    },
-    clearScreen: function () {
-        console.log(clearScreenCode);
-    },
-    getPromptMsg: function (msg) {
-        return colorBgBlack + colorBright + colorFgYellow + msg + colorReset;
-    },
-    getPrettyJson: function (obj) {
-        return JSON.stringify(obj, null, 4);
-    },
-};
+	}
+
+	static error(msg, offset) {
+		if (!offset) offset = 0;
+		console.log(colorBgBlack + colorBright + colorFgRed + this.getTrace() + msg + colorReset);
+	}
+
+	static debug(msg) {
+		console.log(colorBgBlack + colorBright + colorFgGray + this.getTrace() + msg + colorReset);
+	}
+
+	static info(msg) {
+		console.log(colorBgBlack + colorBright + colorFgWhite + this.getTrace() + msg + colorReset);
+	}
+
+	static success(msg) {
+		console.log(colorBgBlack + colorBright + colorFgGreen + this.getTrace() + msg + colorReset);
+	}
+
+	static promptMsg(msg) {
+		console.log(this.getPromptMsg(msg));
+	}
+
+	static clearScreen() {
+		console.log(clearScreenCode);
+	}
+
+	static getPromptMsg(msg) {
+		return colorBgBlack + colorBright + colorFgYellow + msg + colorReset;
+	}
+
+	static getPrettyJson(obj) {
+		return JSON.stringify(obj, null, 4);
+	}
+}
